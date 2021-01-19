d76a84f2185685b29ef3fa8cfc0806ec4a48887e
arborelia <gh@arborelia.net>
2021-01-19 17:34:07 -0500

    new dungeon map

--- scripts/init.lua
+++ scripts/init.lua
@@ -3,6 +3,7 @@ ScriptHost:LoadScript("scripts/settings.lua")
 
 Tracker:AddMaps("maps/maps.json")
 Tracker:AddItems("items/common.json")
+Tracker:AddLocations("locations/locations.json")
+Tracker:AddItems("items/dungeon_proxies.json")
 Tracker:AddLayouts("layouts/tracker.json")
 Tracker:AddLayouts("layouts/tracker_broadcast.json")
-Tracker:AddLocations("locations/locations.json")
